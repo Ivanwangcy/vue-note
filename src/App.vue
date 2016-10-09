a05d1b0a71efaa96ff30011853974ecc1a18c968
Ivan <wcy5211678@163.com>
2016-10-09 12:14:19 +0800

    vue note

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <transition name="fade" mode="out-in">
+      <router-view class="view"></router-view>
+    </transition>
+
+    <div class="footer">
+      <router-link to="/home"><i class="fa fa-desktop"></i></router-link>
+      <router-link to="/about"><i class="fa fa-book"></i></router-link>
+    </div>
+  </div>
+</template>
+
+
+<style lang="sass">
+  @import "./style.scss"
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <div @click="randomTitle">
+    <img class="logo" src="../assets/logo.png"/>
+    <h1>
+    <i class="fa fa-camera-retro"></i>
+    {{ msg }}
+    </h1>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  computed: mapGetters(['msg']),
+  methods: mapActions(['randomTitle'])
+}
+</script>
